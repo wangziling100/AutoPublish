894c3729d01389168b5c032a9ac8b7cb3ad642de
Bob Wang <wangziling1000@gmail.com>
2020-08-06 18:05:30 +0200

    add commit analyser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const github = require('@actions/github')
 const context = github.context
 const wait = require('./wait');
 const cp = require('child_process');
+const semver = require('semver')
 
 
 
@@ -18,6 +19,87 @@ async function run() {
 
     console.log(commit, 'commit');
     console.log(branch, 'branch')
+
+    let tag = null
+    let increace = ''
+    let succeed = false
+    const table = {}
+    table['key'] = [
+      // branch, commit
+      ['master', 'init'],
+      ['master', 'breaking change'],
+      ['master', 'feat'],
+      ['master', 'fix'],
+      ['master', 'merge N'],
+      ['master', 'merge N.N'],
+      ['master', 'merge next'],
+      ['master', 'merge alpha'],
+      ['master', 'merge beta'],
+      ['next', 'init'],
+      ['next', 'feat'],
+      ['next', 'fix'],
+      ['next', 'merge N'],
+      ['next', 'merge N.N'],
+      ['next', 'merge alpha'],
+      ['next', 'merge beta'],
+      ['N', 'feat'],
+      ['N', 'fix'],
+      ['N', 'merge N.N'],
+      ['N', 'merge alpha'],
+      ['N', 'merge beta'],
+      ['N.N', 'fix'],
+      ['alpha', 'feat'],
+      ['alpha', 'fix'],
+      ['alpha', 'merge N'],
+      ['alpha', 'merge N.N'],
+      ['beta', 'feat'],
+      ['beta', 'fix'],
+      ['beta', 'merge N'],
+      ['beta', 'merge N.N']
+      ['N', 'init'],
+      ['N.N', 'init'],
+      ['alpha', 'init'],
+      ['beta', 'init']
+    ]
+    table['value'] = [
+      // version
+      ['major'],
+      ['major'],
+      ['minor'],
+      ['patch'],
+      ['minor'],
+      ['patch'],
+      ['major'],
+      ['major'],
+      ['major'],
+      ['major'],
+      ['minor'],
+      ['patch'],
+      ['minor'],
+      ['patch'],
+      ['minor'],
+      ['minor'],
+      ['minor'],
+      ['patch'],
+      ['minor'],
+      ['minor'],
+      ['patch'],
+      ['prerelease'],
+      ['prerelease'],
+      ['prerelease'],
+      ['prerelease'],
+      ['prerelease'],
+      ['prerelease'],
+      ['prerelease'],
+      ['prerelease'],
+      ['major'],
+      ['minor'],
+      ['prerelease'],
+      ['prerelease']
+    ]
+    switch (branch){
+      case 'master': tag='latest'
+    }
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
@@ -33,9 +115,162 @@ async function run() {
 
 function buffer2String(buffer, key='data'){
   let ret = JSON.stringify(buffer);
-  ret = JSON.parse(ret)
-  ret = ret[key]
-  return String.fromCharCode(...ret)
+  ret = JSON.parse(ret);
+  ret = ret[key];
+  return String.fromCharCode(...ret);
+}
+
+function checkDecisionTable(condition, table){
+  const keys = table.keys;
+  const result = table.value;
+  for (let index in keys){
+    const isEqual = compare(keys[index], condition);
+    if (isEqual) return result[index];
+  }
+  return null;
+}
+
+function compare(a, b){
+  if (a.length!==b.length) return false;
+  for (let index in a){
+    if(a[index]!==b[index]) return false;
+  }
+  return true;
+}
+
+function checkCommitAnalyser(commit, branch){
+  //console.log(commit, '---------------')
+  let workspace = null;
+  let returnCommit = null;
+  const subcommits = commit.split(':');
+  let head = subcommits[0]
+  head = head.replace(/ /g,'');
+  //console.log(head, 'head')
+  switch (head){
+    case 'feat': returnCommit='feat'; break;
+    case 'fix': returnCommit='fix'; break;
+    case 'init': returnCommit='init'; break;
+    case 'breakingchange': 
+      returnCommit='breaking change'; break;
+  }
+  //console.log(returnCommit, 'returnCommit1')
+  let rest = null;
+  let content = null;
+  if (returnCommit===null){
+    const tmp = head.slice(0,5);
+    //console.log(tmp, tmp.length, tmp==='Merge', 'tmp')
+    // merge type
+    if (tmp==='Merge') {
+      returnCommit = 'merge';
+      //rest = commit;
+      content = commit
+    }
+    else return [ null, workspace ];
+  }
+  else {
+    // feat, fix, init, breakingchange type
+    rest = subcommits[1];
+    let tmp = rest.split('@@');
+    if (tmp.length>1) {
+      //console.log(tmp[0], tmp[0].length, 'workspace before')
+      workspace = tmp[0].replace(/ /g, '');
+      //console.log(workspace, workspace.length, 'workspace after')
+      content = tmp.slice(1).join('@@');
+    }
+    else {
+      workspace = 'global';
+      content = rest;
+    }
+  }
+  //console.log(content, 'content')
+
+  if (returnCommit==='merge'){
+    let re = /( |'|")+([0-9]|[a-z]|[A-Z])+@@/
+    workspace = re.exec(content);
+    if (workspace===null) workspace = 'global';
+    else {
+      workspace = workspace[0]
+      workspace = workspace.replace(/ /g, '');
+      workspace = workspace.replace(/@@/g, '');
+      workspace = workspace.replace(/'/g, '');
+      workspace = workspace.replace(/"/g, '');
+    }
+    re = /([0-9])+(.(([0-9])+|x))?.x/;
+    let versions = multiMatch(re, content);
+    //if (!checkVersionValid(versions)) return [ null, workspace ];
+    re = /master|next|alpha|beta/;
+    versions = versions.concat(multiMatch(re, content));
+    //console.log(versions, content, 'versions')
+    if (versions.length===0) return [ null, workspace ]
+    let mergeBranch = findMergeBranch(versions, branch, workspace);
+    if (mergeBranch===null) return [ null, workspace ]
+    mergeBranch = extractVersion(mergeBranch)
+    //console.log(mergeBranch, 'merge branch')
+    if (mergeBranch===null) return [ null, workspace ];
+    switch (mergeBranch){
+      case 'next': returnCommit='merge next'; break;
+      case 'alpha': returnCommit='merge alpha'; break;
+      case 'beta': returnCommit='merge beta'; break;
+      case 'N': returnCommit='merge N'; break;
+      case 'N.N': returnCommit='merge N.N'; break;
+      default: returnCommit=null; break;
+    }
+  }
+  return [ returnCommit, workspace]
+}
+
+function multiMatch(reg, string){
+  let matched='';
+  let result = [];
+  let limit = 50;
+  let cnt = 0
+  while (matched!==null && cnt<limit){
+    matched = reg.exec(string);
+    if (matched!==null) {
+      matched = matched[0]
+      result.push(matched);
+    }
+    else break;
+    string = string.replace(matched, '');
+    cnt++
+  }
+  return result;
+}
+
+function checkVersionValid(versions){
+  for (let version of versions){
+    if(!semver.valid(version)) return false
+  }
+  return true
+}
+
+function findMergeBranch(branches, localBranch, workspace){
+  if (branches.length>2) return null
+  for (let branch of branches){
+    let tmp = branch
+    if (workspace!=='global') tmp = workspace+'@@'+branch
+    if (tmp!==localBranch) return branch
+  }
+  return null
+}
+
+function extractVersion(version){
+  const parts = version.split('.')
+  try{
+    if (parts[1]==='x') return 'N';
+    if (parts[2]==='x') return 'N.N';
+  }
+  catch(err){
+    console.log(err);
+  }
+  return version;
+  
 }
 
 run();
+
+module.exports = {
+  multiMatch: multiMatch,
+  extractVersion: extractVersion,
+  checkCommitAnalyser: checkCommitAnalyser,
+}
--- index.test.js
+++ index.test.js
@@ -2,6 +2,10 @@ const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
+const { multiMatch, 
+        extractVersion,
+        checkCommitAnalyser,
+        } = require('./index');
 
 test('throws invalid number', async () => {
   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
@@ -21,4 +25,97 @@ test('test runs', () => {
   const ip = path.join(__dirname, 'index.js');
   //console.log(ip, process.env)
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+});
+
+test('test multiMatch', () => {
+  const commit1 = 'merge 1.2.x to master'
+  const commit2 = 'merge 1.2.x to 1.x'
+  const commit3 = 'merge v1.2.x to v1.x'
+  const re = /([0-9])+(.(([0-9])+|x))?.x/
+  console.log(typeof(multiMatch))
+  multiMatch(re, commit1)
+  expect(multiMatch(re, commit1)).toEqual(['1.2.x'])
+  expect(multiMatch(re, commit2)).toEqual(['1.2.x', '1.x'])
+  expect(multiMatch(re, commit3)).toEqual(['1.2.x', '1.x'])
+  //console.log(multiMatch(re, commit1))
+  //console.log(multiMatch(re, commit2))
+  //console.log(multiMatch(re, commit3))
+
+})
+
+test('test extractVersion', () => {
+  const version1 = '1.2.x'
+  const version2 = '1.x'
+  const version3 = 'master'
+  const version4 = '1.2.3'
+  expect(extractVersion(version1)).toBe('N.N')
+  expect(extractVersion(version2)).toBe('N')
+  expect(extractVersion(version3)).toBe('master')
+  expect(extractVersion(version4)).toBe('1.2.3')
+}) 
+
+test('test checkCommitAnalyser', ()=>{
+  const commit1 = 'init: init commit'
+  const commit2 = 'breaking change: new feature'
+  const commit3 = 'feat: new feature'
+  const commit4 = 'fix: bug #5'
+  const commit5 = 'init: module-a @@ init commit'
+  const commit6 = 'breaking change: module-b @@ new feature'
+  const commit7 = 'feat: module c @@ new feature'
+  const commit8 = 'fix: module d @@ bug #5'
+  const commit9 = 'Merge pull request #98 from XXX/v1.0.x'
+  const commit10 = "Merge branch 'master' of github.com:XXX/ABC"
+  const commit11 = "Merge branch 'abc@@v1.0.x'"
+  const commit12 = "Merge branch abc@@v1.x"
+  const commit13 = "Merge branch abc@@v1.x.3"
+  const commit14 = "Merge branch next"
+  const commit15 = "Merge branch alpha"
+  const commit16 = "Merge branch beta"
+  const commit17 = "Merge branch abc@@next"
+  const commit18 = "Merge branch abc@@alpha"
+  const commit19 = "Merge branch abc@@beta"
+  expect(checkCommitAnalyser(commit1, 'master'))
+  .toEqual(['init', 'global'])
+  expect(checkCommitAnalyser(commit2, 'master'))
+  .toEqual(['breaking change', 'global'])
+  expect(checkCommitAnalyser(commit3, 'master'))
+  .toEqual(['feat', 'global'])
+  expect(checkCommitAnalyser(commit4, 'master'))
+  .toEqual(['fix', 'global'])
+  expect(checkCommitAnalyser(commit5, 'master'))
+  .toEqual(['init', 'module-a'])
+  expect(checkCommitAnalyser(commit6, 'master'))
+  .toEqual(['breaking change', 'module-b'])
+  expect(checkCommitAnalyser(commit7, 'master'))
+  .toEqual(['feat', 'modulec'])
+  expect(checkCommitAnalyser(commit8, 'master'))
+  .toEqual(['fix', 'moduled'])
+  expect(checkCommitAnalyser(commit9, 'master'))
+  .toEqual(['merge N.N', 'global'])
+  expect(checkCommitAnalyser(commit10, 'master'))
+  .toEqual([null, 'global'])
+  expect(checkCommitAnalyser(commit10, 'next'))
+  .toEqual([null, 'global'])
+  expect(checkCommitAnalyser(commit11, 'master'))
+  .toEqual(['merge N.N', 'abc'])
+  expect(checkCommitAnalyser(commit12, 'master'))
+  .toEqual(['merge N', 'abc'])
+  expect(checkCommitAnalyser(commit13, 'master'))
+  .toEqual(['merge N', 'abc'])
+  expect(checkCommitAnalyser(commit14, 'master'))
+  .toEqual(['merge next', 'global'])
+  expect(checkCommitAnalyser(commit14, 'next'))
+  .toEqual([null, 'global'])
+  expect(checkCommitAnalyser(commit15, 'master'))
+  .toEqual(['merge alpha', 'global'])
+  expect(checkCommitAnalyser(commit16, 'master'))
+  .toEqual(['merge beta', 'global'])
+  expect(checkCommitAnalyser(commit17, 'master'))
+  .toEqual(['merge next', 'abc'])
+  expect(checkCommitAnalyser(commit18, 'master'))
+  .toEqual(['merge alpha', 'abc'])
+  expect(checkCommitAnalyser(commit19, 'master'))
+  .toEqual(['merge beta', 'abc'])
+  expect(checkCommitAnalyser(commit19, 'abc@@beta'))
+  .toEqual([null, 'abc'])
 })
